03af9929cf8f27dc1a1281c867346c6feac36ed5
Cody Spratford <koreanpanda345@gmail.com>
2024-08-05 12:38:15 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'thetrainercorner',
-	host: 'localhost',
+	host: 'sim.pokemonnext.com',
 	port: 8000,
 	httpport: 80,
 	altport: 80,
